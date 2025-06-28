b4bb65aa08a28a2806de0964f2b7431f16117824
Jeegar Goyani <39588735+jigo961257@users.noreply.github.com>
2025-06-28 14:37:20 +0530

    update

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -210,7 +210,7 @@ const getAllUsers = async (req: any, res: any) => {
       return errorResponse(res, "Error fetching users", 500);
     }
 
-    return successResponse(res, "Users fetched successfully", data.filter(e => e.role_id !== "97718f98-1d79-46a0-9e0f-e39ada2d665c"));
+    return successResponse(res, "Users fetched successfully", data.filter(e => e.role_id !== "97718f98-1d79-46a0-9e0f-e39ada2d665c") || []);
   } catch (err) {
     console.error(err);
     return errorResponse(res, "Internal Server Error", 500);
